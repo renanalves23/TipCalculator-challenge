de3628f9fa25cf5f9a9eafb732aa870570d0f0c6
renan <renanalves23@outlook.com>
2019-09-30 13:04:58 -0300

    chore: Commit inicial

---
+++ challenge03.js
@@ -0,0 +1,12 @@
+ function tipCalculator(bill) {
+  var percentage;
+      if (bill < 50) {
+      percentage = 0.2;
+    }else if (bill >= 50 && bill <= 200){
+      percentage = 0.15;
+    }else {
+      percentage = 0.1;
+    }
+    return percentage * bill;
+  }
+console.log(tipCalculator(250));
